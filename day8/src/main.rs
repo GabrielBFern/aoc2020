82cb1f4118ad1bbecd9c9a76275fd10ee44f8dc2
Gabriel Fernandes <fernandesbgabriel@gmail.com>
2021-01-12 21:28:47 -0300

    Day 8 part 2 optmization

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::convert::TryFrom;
 use std::str::FromStr;
 
@@ -9,10 +9,13 @@ use nom::multi::separated_list1;
 use nom::sequence::tuple;
 use nom::{Finish, IResult};
 
-#[derive(Debug, Clone)]
-struct Program {
+#[derive(Default)]
+struct Cpu {
     accumulator: isize,
     program_counter: usize,
+}
+#[derive(Debug, Clone)]
+struct Program {
     instructions: Vec<OpCode>,
 }
 #[derive(Debug, PartialEq, Clone)]
@@ -23,76 +26,136 @@ enum OpCode {
 }
 
 fn main() {
-    let input = include_str!("../../input/d8");
+    let input = include_str!("../../input/d8large");
     part1(input);
     part2(input);
 }
 
 fn part1(input: &str) {
     let program = parse_program(input).expect("invalid input");
-    let (_, acu) = program.run_program();
+    let (_, acu) = Cpu::default().run_program(&program);
     println!("Part1: {:}", acu);
 }
 
-// TODO: Remake this, brute force is not acceptable
 fn part2(input: &str) {
-    let program = parse_program(input).expect("invalid input");
-    let change = program
-        .instructions
-        .iter()
-        .enumerate()
-        .filter(|(_, m)| !matches!(m, OpCode::Acc(_)))
-        .find(|(f, _)| {
-            let mut p = program.clone();
-            p.change_op(*f);
-            p.run_program().0.is_none()
-        });
-    let mut p = program.clone();
-    p.change_op(change.unwrap().0);
-    let (_, acu) = p.run_program();
+    let mut program = parse_program(input).expect("invalid input");
+    let end_point = generate_endpoints(&program);
+    let mut cpu = Cpu::default();
+    loop {
+        let op = cpu.get_instruction(&program).unwrap();
+        if let Some(changed_op) = op.change() {
+            let destiny = cpu.calculate_destination(&changed_op);
+
+            if end_point.contains(&destiny) {
+                program.instructions[cpu.program_counter] = changed_op;
+                break;
+            }
+        }
+        cpu.tick(&program);
+    }
+    let (_, acu) = Cpu::default().run_program(&program);
     println!("Part2: {:}", acu);
 }
 
-impl Program {
-    fn tick(&mut self) -> Option<&OpCode> {
-        let op = self.instructions.get(self.program_counter)?;
-        match op {
-            OpCode::Acc(i) => {
-                self.accumulator += i;
-                self.program_counter += 1;
+fn generate_endpoints(program: &Program) -> HashSet<usize> {
+    let mut destinations: HashMap<usize, HashSet<usize>> = HashMap::new();
+    let length = program.instructions.len() as isize;
+    program
+        .instructions
+        .iter()
+        .enumerate()
+        .map(|(ori, op)| {
+            let des: isize = match op {
+                OpCode::Jmp(x) => ori as isize + x,
+                _ => (ori + 1) as isize,
+            };
+            let des: usize = match des {
+                x if x < 0 => 0,
+                x if x >= length => program.instructions.len() - 1,
+                x => x as usize,
+            };
+            (ori, des)
+        })
+        .for_each(|(origem, destino)| {
+            let des = destinations.get_mut(&destino);
+            if let Some(set) = des {
+                set.insert(origem);
+            } else {
+                let mut set = HashSet::new();
+                set.insert(origem);
+                destinations.insert(destino, set);
             }
-            OpCode::Jmp(i) => {
-                self.program_counter = if i.is_negative() {
-                    self.program_counter.checked_sub(i.wrapping_abs() as usize)
-                } else {
-                    self.program_counter.checked_add(*i as usize)
-                }?;
-            }
-            OpCode::Nop(_) => {
-                self.program_counter += 1;
-            }
-        }
+        });
+    let mut end_points = HashSet::new();
+    let mut nodes_left = vec![];
+    nodes_left.push(program.instructions.len() - 1);
+    while let Some(x) = nodes_left.pop() {
+        end_points.insert(x);
+        destinations.get(&x).iter().for_each(|e| {
+            e.iter().for_each(|d| {
+                if !end_points.contains(d) {
+                    nodes_left.push(*d);
+                }
+            })
+        });
+    }
+    end_points
+}
+
+impl Cpu {
+    fn tick<'a>(&mut self, program: &'a Program) -> Option<&'a OpCode> {
+        let op = self.get_instruction(program)?;
+        self.accumulator = self.calcualte_accu(op);
+        self.program_counter = self.calculate_destination(op);
         Some(op)
     }
 
-    fn change_op(&mut self, index: usize) {
-        self.instructions[index] = match self.instructions.get(index).unwrap() {
-            OpCode::Nop(i) => OpCode::Jmp(*i),
-            OpCode::Jmp(i) => OpCode::Nop(*i),
-            _ => unimplemented!(),
-        };
+    fn get_instruction<'a>(&self, program: &'a Program) -> Option<&'a OpCode> {
+        program.instructions.get(self.program_counter)
+    }
+
+    fn calcualte_accu(&self, op: &OpCode) -> isize {
+        if let OpCode::Acc(i) = op {
+            self.accumulator + i
+        } else {
+            self.accumulator
+        }
+    }
+
+    fn calculate_destination(&self, op: &OpCode) -> usize {
+        match op {
+            OpCode::Acc(_) | OpCode::Nop(_) => self.program_counter + 1,
+            OpCode::Jmp(i) => if i.is_negative() {
+                self.program_counter
+                    .checked_sub(i.wrapping_abs() as usize)
+                    .or(Some(0))
+            } else {
+                self.program_counter.checked_add(*i as usize)
+            }
+            .unwrap(),
+        }
     }
 
     // Run the program and return if found loop and the last accumulator
-    fn run_program(mut self) -> (Option<()>, isize) {
+    fn run_program(mut self, program: &Program) -> (Option<()>, isize) {
         let mut instruction_viewed: HashSet<usize> = HashSet::new();
         loop {
             if !instruction_viewed.insert(self.program_counter) {
                 break (Some(()), self.accumulator);
             }
-            if self.tick().is_none() {
+            if self.tick(program).is_none() {
                 break (None, self.accumulator);
             }
+        }
+    }
+}
+
+impl OpCode {
+    const fn change(&self) -> Option<Self> {
+        match self {
+            Self::Jmp(x) => Some(Self::Nop(*x)),
+            Self::Nop(x) => Some(Self::Jmp(*x)),
+            Self::Acc(_) => None,
         }
     }
 }
@@ -113,8 +176,6 @@ impl TryFrom<(&str, isize)> for OpCode {
 fn parse_program(input: &str) -> Result<Program, String> {
     match all_consuming(separated_list1(line_ending, parse_op))(input).finish() {
         Ok((_remaining, op_codes)) => Ok(Program {
-            accumulator: 0,
-            program_counter: 0,
             instructions: op_codes,
         }),
         Err(_) => Err("Parse error".into()),
@@ -154,7 +215,7 @@ mod test_super {
     fn test_example_1() {
         let input = "nop +0\nacc +1\njmp +4\nacc +3\njmp -3\nacc -99\nacc +1\njmp -4\nacc +6";
         let program = parse_program(input).unwrap();
-        let result = program.run_program();
+        let result = Cpu::default().run_program(&program);
         assert_eq!(result, (Some(()), 5));
     }
 
@@ -162,7 +223,7 @@ mod test_super {
     fn test_example_2() {
         let input = "nop +0\nacc +1\njmp +4\nacc +3\njmp -3\nacc -99\nacc +1\nnop -4\nacc +6";
         let program = parse_program(input).unwrap();
-        let result = program.run_program();
+        let result = Cpu::default().run_program(&program);
         assert_eq!(result, (None, 8));
     }
 }
